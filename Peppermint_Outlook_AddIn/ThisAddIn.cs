7a591431fd34f7dbb4b59af486d753b0b8394327
Sanjay Karia <sanjaykaria@gmail.com>
2015-10-07 11:09:28 +0530

    Change font to "Open Sans"

--- Peppermint_Outlook_AddIn/ThisAddIn.cs
+++ Peppermint_Outlook_AddIn/ThisAddIn.cs
@@ -19,8 +19,8 @@ namespace Peppermint_Outlook_AddIn
         public static Outlook.MailItem theCurrentMailItem;
         public static Outlook.Application outlookApp;
         public static string AttachmentFilePath;
-        public static string PEPPERMINT_NEW_EMAIL_HTML_BODY = "Here's my message<BR> Reply via <a href=Peppermint.com>Peppermint.com</a><BR><BR>";
-        public static string PEPPERMINT_REPLY_MAIL_HTML_BODY = "I sent you an audio reply with <a href=Peppermint.com>Peppermint.com</a><BR><BR>";
+        public static string PEPPERMINT_NEW_EMAIL_HTML_BODY = "<FONT SIZE=3 FACE=\"Open Sans\">Here's my message<BR> Reply via <a href=Peppermint.com>Peppermint.com</a><BR><BR></FONT>";
+        public static string PEPPERMINT_REPLY_MAIL_HTML_BODY = "<FONT SIZE=3 FACE=\"Open Sans\">I sent you an audio reply with <a href=Peppermint.com>Peppermint.com</a><BR><BR></FONT>";
         public static string PEPPERMINT_WEBSITE = "Peppermint.com";
 
         public static string PEPPERMINT_NEW_MAIL_SUBJECT = "I sent you a voicemail message";
