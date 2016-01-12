b558da862a3d4d3f79ec46e67701bf24d444fb45
Sanjay Karia <sanjaykaria@gmail.com>
2016-01-12 16:22:39 +0530

    Rectify image size

--- Peppermint_Outlook_AddIn/frmAbout.Designer.cs
+++ Peppermint_Outlook_AddIn/frmAbout.Designer.cs
@@ -74,6 +74,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(25, 9);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(41, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
